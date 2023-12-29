cf107b0a00307556fed263be41f19941808ca6b3
bee-honey <honeybeeprogrammer@gmail.com>
2023-12-29 00:42:52 -0600

    Load balanced on the client side, to pick any of the instances of inventory instead of getting confused and failing

--- ecom/order-service/src/main/java/com/nakeer/orderservice/service/OrderService.java
+++ ecom/order-service/src/main/java/com/nakeer/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -38,7 +38,7 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
                 .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
